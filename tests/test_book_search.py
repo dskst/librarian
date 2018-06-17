29001d28eaf90b9540fbe013a2ef4adb103d2180
dskst <grandevivo9-dskst@yahoo.co.jp>
2018-06-17 21:46:23 +0900

    Add testcase for BookSearch

--- tests/test_book_search.py
+++ tests/test_book_search.py
@@ -7,6 +7,7 @@ import luigi
 import luigi.notifications
 from luigi.mock import MockTarget
 from helpers import LuigiTestCase
+import re
 
 import __builtin__
 from tasks.library_books import BookSearch
@@ -26,5 +27,26 @@ class BookSearchTest(LuigiTestCase):
 
     def test_input_isbn_when_input_value_is_returned(self):
         isbn = ''
+        original = __builtin__.raw_input
         __builtin__.raw_input = lambda _: '0123456789012'
         self.assertEqual('0123456789012', BookSearch(luigi.task).input_isbn(isbn))
+        __builtin__.raw_input = original
+
+    def test_search_when_isbn_is_empty(self):
+        with self.assertRaises(RuntimeError) as cm:
+            BookSearch(luigi.task).search('')
+        self.assertEquals(cm.exception.message, 'ISBN must not empty')
+
+    def test_search_when_isbn_different_models(self):
+        with self.assertRaises(RuntimeError) as cm:
+            BookSearch(luigi.task).search('1')
+        self.assertEquals(cm.exception.message, 'Book is not found')
+
+    def test_search_when_isbn_not_exists(self):
+        with self.assertRaises(RuntimeError) as cm:
+            BookSearch(luigi.task).search('9999999999999')
+        self.assertEquals(cm.exception.message, 'Book is not found')
+
+    def test_search_when_normal(self):
+        response = BookSearch(luigi.task).search('9784873117386')
+        self.assertEquals(1, response['totalItems'])
